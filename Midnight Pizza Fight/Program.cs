5fdb2992c3403c8911024a980acae2a8fb6432a8
Sayan11071998 <sayannandi11071998@gmail.com>
2024-10-06 13:24:59 +0530

    Display the Story

---
+++ Midnight Pizza Fight/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("\n==================================================================");
+        Console.WriteLine("             🍕 MIDNIGHT PIZZA FIGHT 🍕             ");
+        Console.WriteLine("===================================================================");
+        Console.WriteLine("\nIn a bustling pizzeria on a busy Friday night...");
+        Console.WriteLine("-------------------------------------------------------------------");
+        Console.WriteLine("You, the Dough Master, created your magnum opus - the perfect pizza🤌. Suddenly, a sneaky Crust Bandit snatches your masterpiece!");
+        Console.WriteLine("-------------------------------------------------------------------");
+        Console.WriteLine("Fuelled by the passion for your craft, you give chase...");
+        Console.WriteLine("-------------------------------------------------------------------");
+        Console.WriteLine("Through winding alleys and crowded streets, you pursue the pizza pilferer. Finally, the thief is cornered in a dead-end alley. It's time to recover your stolen slice!");
+        Console.WriteLine("-------------------------------------------------------------------");
+        Console.WriteLine("                               FIGHT                               ");
+    }
+}
